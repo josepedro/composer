195f6dd6f4c9834157548b0771869a463dfd0619
Jose Pedro <1jpsneto@gmail.com>
2014-04-09 15:29:07 -0300

    =D

--- abordagem_C/mountSTL.c
+++ abordagem_C/mountSTL.c
@@ -9,23 +9,23 @@ void end_draw(FILE *arquivo){
 	fprintf(arquivo,"endsolid");
 }
 
-void draw_cube(FILE *arquivo/*, float positionX, float positionY, float positionZ, float height*/){
-	float position[] = {1.000000,1.000000,1.000000};
+void draw_cube(FILE *arquivo, float positionX, float positionY, float positionZ, float height){
+	float position[] = {4.000000,4.000000,4.000000};
 	
 
 	//Base face YZ
 	fprintf(arquivo,"facet normal -1.000000e+000 0.000000e+000 0.000000e+000\n\t\t");
 	fprintf(arquivo,"outer loop\n\t\t");
-	fprintf(arquivo,"vertex -4.000000e+002 %.6fe+002 %.6fe+002\n\t\t",4.000000,4.000000);
-	fprintf(arquivo,"vertex -4.000000e+002 %.6fe+002 -4.000000e+002\n\t\t",4.000000);
-	fprintf(arquivo,"vertex 0.000000e+000 -4.000000e+002 %.6fe+002\n\t\t",4.000000);
+	fprintf(arquivo,"vertex 0.000000e+000 %.6fe+002 %.6fe+002\n\t\t",position[1],position[2]);
+	fprintf(arquivo,"vertex 0.000000e+000 %.6fe+002 0.000000e+000\n\t\t",position[1]);
+	fprintf(arquivo,"vertex 0.000000e+000 0.000000e+000 %.6fe+002\n\t\t",position[2]);
 	fprintf(arquivo,"endloop\n\t");
 	fprintf(arquivo,"endfacet\n\t");
 	
 	fprintf(arquivo,"facet normal -1.000000e+000 0.000000e+000 0.000000e+000\n\t\t");
 	fprintf(arquivo,"outer loop\n\t\t");
-	fprintf(arquivo,"vertex 0.000000e+000 0.000000e+000 %.6fe+002\n\t\t",4.000000);
-	fprintf(arquivo,"vertex 0.000000e+000 %.6fe+002 0.000000e+000\n\t\t",4.000000);
+	fprintf(arquivo,"vertex 0.000000e+000 0.000000e+000 %.6fe+002\n\t\t",position[2]);
+	fprintf(arquivo,"vertex 0.000000e+000 %.6fe+002 0.000000e+000\n\t\t",position[1]);
 	fprintf(arquivo,"vertex 0.000000e+000 0.000000e+000 0.000000e+000\n\t\t");
 	fprintf(arquivo,"endloop\n\t");
 	fprintf(arquivo,"endfacet\n\t");
