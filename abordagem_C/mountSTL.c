eaa9241a8f335b51c6259db73abc440ffb52fd45
Jose Pedro <1jpsneto@gmail.com>
2014-05-30 23:33:37 -0300

    Change resolution 1 cm por cube

--- abordagem_C/mountSTL.c
+++ abordagem_C/mountSTL.c
@@ -15,13 +15,13 @@ void draw_cube_20mm(){
     int iteratorX;
     int drawY = 0;
     int drawX = 0;
-    for(iteratorY = 0; iteratorY < 200; iteratorY++){
-        for(iteratorX = 0; iteratorX < 200; iteratorX++){
-            draw_rectangle(arquivo, drawX, drawY, 0, 200, 0.1);
-            drawX = drawX + 1.000000*10;
+    for(iteratorY = 0; iteratorY < 2; iteratorY++){
+        for(iteratorX = 0; iteratorX < 2; iteratorX++){
+            draw_rectangle(arquivo, drawX, drawY, 0, 1.5, 10);
+            drawX = drawX + 1.000000*1000;
         }
         drawX = 0;
-       drawY = drawY + 1.000000*10;
+       drawY = drawY + 1.000000*1000;
     } 
     //draw_rectangle(arquivo, drawX, drawY, 0, 1, 20000);   
 	//draw_cube(arquivo,0.5,0.5,0,0.5);
@@ -35,9 +35,9 @@ void draw_cube_20mm(){
 }
 
 void draw_signal_sum(){
-	int width = 100; //20 mm or 2 cm; 10 = 1 mm; 1 = 0.1 mm with 0.1 mm resolution
-	int roof = 100;
-    int ground = 50;
+	int width = 5; //20 mm or 2 cm; 10 = 1 mm; 1 = 0.1 mm with 0.1 mm resolution
+	int roof = 3;
+    int ground = 0.5;
 	int band = width * 20/100;
 	int limit_less = (width - band)/2;
 	int limit_upper = width - limit_less;
@@ -51,15 +51,15 @@ void draw_signal_sum(){
     for(iteratorY = 0; iteratorY < width; iteratorY++){
         for(iteratorX = 0; iteratorX < width; iteratorX++){
         	if (iteratorX >= limit_less && iteratorX < limit_upper || iteratorY >= limit_less && iteratorY < limit_upper){
-        		draw_rectangle(arquivo, drawX, drawY, 0, roof, 1);
+        		draw_rectangle(arquivo, drawX, drawY, 0, roof, 10);
         	}
         	else{
-        		draw_rectangle(arquivo, drawX, drawY, 0, ground, 1);
+        		draw_rectangle(arquivo, drawX, drawY, 0, ground, 10);
         	}
-            drawX = drawX + 1.000000*100;
+            drawX = drawX + 1.000000*1000;
         }
         drawX = 0;
-       drawY = drawY + 1.000000*100;
+       drawY = drawY + 1.000000*1000;
     } 
 
 	end_draw(arquivo);
